6daf60bd39c2c3692b0612e79d2c623c058f0f84
law <lawiparadise@gmail.com>
2016-05-21 05:13:32 +0900

    ee

--- app/src/main/java/com/law/hongcaphelp/broad/MyBroadcastReceiver.java
+++ app/src/main/java/com/law/hongcaphelp/broad/MyBroadcastReceiver.java
@@ -11,7 +11,7 @@ import com.law.hongcaphelp.my_service.MyService2;
  * Created by GDGO on 2016-04-13.
  */
 public class MyBroadcastReceiver extends BroadcastReceiver {
-
+//
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals("android.intent.action.ACTION_POWER_CONNECTED")){
